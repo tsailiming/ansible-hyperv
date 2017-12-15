c426646b56ecce1bc692251e5ede7ba55210bf4d
Tsai Li Ming <tsai.liming@gmail.com>
2017-12-16 00:59:03 +0800

    We now can provision a base template, by copying vhd that consists of: * IIS + OrchardCMS preinstalled * MS-SQL 2014 Express
    The use case we are demonstrating here is that:
    * We can provision a stock image
    * Restore the app settings: OrchardCMS's App_Data directory
    * Restore the db backup (Created using SQL Server Management Studio (SSMS))

---
+++ library/win_mssql_database.ps1
@@ -0,0 +1,42 @@
+#!powershell
+
+# WANT_JSON
+# POWERSHELL_COMMON
+
+Set-StrictMode -Version 2
+$ErrorActionPreference = "Stop"
+
+$parsed_args = Parse-Args $args $true
+
+$result = @{changed=$false}
+
+$server_name = Get-AnsibleParam $parsed_args "server_name" -default "(local)"
+$db_name = Get-AnsibleParam $parsed_args "db_name" -failifempty $result
+$state = Get-AnsibleParam $parsed_args "state" -default "present" -ValidateSet @("present","absent")
+$check_mode = Get-AnsibleParam $parsed_args "_ansible_check_mode" -default $false
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server $server_name
+
+$db = $server.Databases[$db_name]
+
+If(-not $db -and $state -eq "present") {
+    $result.changed = $true
+
+    If(-not $check_mode) {
+        # DB doesn't exist, create it
+        $db = New-Object Microsoft.SqlServer.Management.Smo.Database @($server, $db_name)
+        $db.Create()
+    }
+}
+ElseIf($db -and $state -eq "absent") {
+    $result.changed = $true
+
+    If(-not $check_mode) {
+        $db.Drop()
+    }
+}
+
+return $result | ConvertTo-Json -Depth 99
+
